245670b155e6a029a0338589735031af2c1c0480
Kravchuk Max <maxkravchuk139@gmail.com>
2024-12-09 10:20:42 +0200

    Add files via upload

---
+++ Maksim PR 1 (1).py
@@ -0,0 +1,14 @@
+hello = "Hello World"
+name = "Maksum"
+prizv = "Kravchuk"
+age = 16
+
+print(hello)
+print(name)
+print(prizv)
+print(age)
+
+print(type(hello))
+print(type(name))
+print(type(prizv))
+print(type(age))
